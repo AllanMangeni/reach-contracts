d1ff190f18d4b77e87550ae798b4aa57b7aace7d
Allan mang'eni <allanwanyonyi@protonmail.com>
2022-01-28 07:48:52 +0000

    config .mjs file

--- index.mjs
+++ index.mjs
@@ -10,12 +10,25 @@ const stdlib = loadStdlib(process.env);
     const ctcAlice = accAlice.contract(backend);
     const ctcBob = await stdlib.newTestAccount(startingBalance);
 
+    const HAND = ['Rock', 'Paper', 'Scissors'];
+    const OUTCOME = ['Bob Wins', 'Draw', 'Alice Wins'];
+    const Player = (Who) => ({
+        getHand: () => {
+            const hand = Math.floor(Math.random() * 3);
+            console.log('${Who} played ${HAND[hand]}');
+            return hand;
+        },
+        seeOutcome: (outcome) => {
+            console.log('${Who} saw outcome ${OUTCOME[outcome]}');
+        },
+    });
+    
     await Promise.all([
         ctcAlice.p.Alice({
-            // implement Alice interact project here
+            ...Player('Alice'),
         }),
         ctcBob.p.Bob({
-            // implement Bob's interact object here
+            ...Player('Bob'),
         }),
     ]);
-})();
+});
